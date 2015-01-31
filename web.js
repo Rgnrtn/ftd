d719312dc76dfb0d3bfa22bef1e7491f9eff0bb2
Donglin Pu <Dongminator@Donglins-MacBook-Pro.local>
2015-01-30 21:30:04 -0800

    weibo test

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var app = express();
+var http = require('http');
+var https = require('https');
 
-app.set('port', (process.env.PORT || 5000))
+
+var server = http.createServer(app);
+
 
 app.get('/', function(request, response) {
 	response.sendfile(__dirname+'/index.html');
@@ -70,6 +74,8 @@ app.get('/facebook_login', function(request, response) {
 	response.send("facebook login");
 });
 
-app.listen(app.get('port'), function() {
+
+var port = process.env.PORT || 5000;
+server.listen(port, function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
