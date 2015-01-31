bee85598775affb22dc5ee309082ef3f8a9379be
Donglin Pu <Dongminator@Donglins-MacBook-Pro.local>
2015-01-31 00:54:54 -0800

    refresh files

--- web.js
+++ web.js
@@ -2,11 +2,14 @@ var express = require('express')
 var app = express();
 var http = require('http');
 var https = require('https');
-
+var bodyParser = require('body-parser');
 
 var server = http.createServer(app);
 
-
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/', function(request, response) {
 	response.sendfile(__dirname+'/index.html');
@@ -84,13 +87,99 @@ app.get('/facebook_login', function(request, response) {
 });
 
 
-app.get('/facebook_login1', function(request, response) {
+app.post('/facebook_token', function(request, response) {
+	console.log(request.body.access_token);
+	var code = request.param('access_token');
 	
+//	console.log(code);
 	
-	response.send("facebook login1");
+	response.send("thanks for the token");
 });
 
 
+
+
+
+
+
+
+app.get('/twitter_login', function(request, response) {
+	
+	
+	response.send("facebook login");
+});
+
+/*
+ * https://dev.twitter.com/docs/auth/application-only-auth 
+ * Step 2: obtain a bearer token
+ */
+app.get('/twitter_login2', function(request, response){
+	var headers = { 
+		    'Authorization': 'Basic ' + credentials.base64,
+		    'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8' 
+	};
+	var options = {
+			  hostname: 'api.twitter.com',
+			  path: "/oauth2/token",
+			  method: 'POST',
+			  headers: headers
+	};
+	
+	var req = https.request(options, function(res) {
+		res.on('data', function (chunk) {
+			response.send(chunk);
+		});
+	});
+	
+	req.write('grant_type=client_credentials');
+	req.end();
+});
+
+/*
+ * Step 3: Authenticate API requests with the bearer token
+ * twitter_login3 is the actual query. need to rename it to query or something else.
+ * Search API: https://stream.twitter.com/1.1/statuses/filter.json
+ */
+app.get('/twitter_login3', function(request, response){
+	var bearer_token = request.param('bearer_token');
+	var query_string = request.param('q');
+	
+	// !! Do not forget to add "Bearer " before bearer_token!!!
+	var headers = { 
+		    'Authorization': "Bearer " + bearer_token,
+	};
+	
+	var options = {
+			hostname: 'api.twitter.com',
+			path: '/1.1/search/tweets.json' + query_string, // query_string already has "?q="
+			method: 'GET',
+			headers: headers
+	};
+	var body = "";
+	var req = https.request(options, function(res) { // res is IncomingMessage help: http://nodejs.org/api/http.html#http_http_incomingmessage
+		// res.statusCode
+		res.setEncoding("utf8");
+		res.on('data', function (chunk) {// this happens multiple times! So need to use 'body' to collect all data
+			body += chunk;
+		});
+		
+		var data="";
+		res.on('end', function () { // when we have full 'body', convert to JSON and send back to client.
+			try {
+				data = JSON.parse(body);
+		    } catch (er) {
+		    	// something wrong with JSON
+		    	response.statusCode = 400;
+		    	return response.end('error: ' + er.message);
+		    }
+
+		    // write back response json
+		    response.send(data);
+		    response.end();
+		  });
+	});
+	req.end();
+});
 
 
 
